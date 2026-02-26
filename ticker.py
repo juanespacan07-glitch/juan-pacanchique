7d3f97e56b471d27a8f43a532b9e3f68ad33d2c7
juan pacanchique <juanespacan07@gmail.com>
2026-02-26 11:44:11 -0500

    Implentamos REPORTE

--- ticker.py
+++ ticker.py
@@ -1,8 +1,23 @@
 
 estudiantes = [
-{"nombre" : "Juan", "notas":[3.0, 4.5, 4.0]},
+{"nombre" : "Juan", "Notas":[3.0, 4.5, 4.0]},
 {"nombre" : "Myraa", "Notas":[4.0, 5.0, 3.5]},
 {"nombre" : "Ryu", "Notas":[3.0, 4.2, 3.5]}
 ]
 print("---Datos cargados---")
 print(f"Total de estudiantes: {len(estudiantes)}")
+
+def reporte(estudiantes):
+    print("---Reporte Final---")
+    for est in estudiantes:
+        suma = sum(est["Notas"])
+        prom = suma / len(est["Notas"])
+
+        if prom >=3.0:
+           estado = "Aprobado"
+        else:
+            estado = "Reprobado"
+
+        print(f"estudiantes: {est["nombre"]} su estado es {estado}")
+
+reporte(estudiantes)
