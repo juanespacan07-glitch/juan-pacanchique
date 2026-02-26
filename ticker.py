ba3a553b491d890144e27955fde88fdae3a7323d
juan pacanchique <juanespacan07@gmail.com>
2026-02-26 10:45:11 -0500

    iniciando proyecto Tyker

---
+++ ticker.py
@@ -0,0 +1,8 @@
+
+estudiantes = [
+{"nombre" : "Juan", "notas":[3.0, 4.5, 4.0]},
+{"nombre" : "Myraa", "Notas":[4.0, 5.0, 3.5]},
+{"nombre" : "Ryu", "Notas":[3.0, 4.2, 3.5]}
+]
+print("---Datos cargados---")
+print(f"Total de estudiantes: {len(estudiantes)}")
