392164d9c8b61c40c09ac8b7b31063ae0fd08222
juan pacanchique <juanespacan07@gmail.com>
2026-03-04 11:22:33 -0500

    Funciones de la 2 a la 4

---
+++ invetario.py
@@ -0,0 +1,58 @@
+def main():
+    
+    inventario = []
+
+    while True:
+        opcion = input("Seleccione una opcion")
+        
+        if opcion == "1":
+            print("---NUEVO PRODUCTO---")
+            nombre = input("Nombre: ")
+
+            try:
+                precio = float(input("Ingrese el precio: "))
+                stock = int(input("ingrese la cantiad: "))
+            
+                producto = {
+                    "Nombre" : nombre,
+                    "Precio" : precio,
+                    "cantidad" : stock
+                }
+
+                inventario.append(producto)
+
+            except ValueError:
+                print("Error, el precio y stock deben ser numeros.")
+
+        elif opcion =="2":
+            print("---INVENTARIO ACTUAL---")
+
+            if len(inventario) == 0:
+                print("inventario vacio.")
+            else:
+                for item in inventario:
+                    print(f"Producto: {item["Nombre"]}, el precio es {item["Precio"]} y su stock es de: {item["Cantidad"]}")    
+       
+        elif opcion =="3":
+            busqueda = input("Ingrese el nombre a buscar: ").lower()
+            encontrar = False
+
+            for item in inventario:
+                if item ["Nombre"].lower() == busqueda:
+                    print(f"Encontrado: Precio ${item["Precio"]} - Stock: {item["Cantidad"]}")
+                    encontrar = True
+                    break
+            
+            if not encontrar:
+                print("Producto no encontrado")
+
+        elif opcion == "4":
+
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opcion invalida, intente de nuevo.")                
+
+
+if __name__=="__main__":
+    main()
